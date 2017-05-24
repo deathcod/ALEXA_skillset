ec2ff3774d9fd6384e61fe62b86606c0ade6e05d
deathcod <chinmay.rakshit@gmail.com>
2017-05-24 17:30:07 +0530

    new change

--- LAMBDA/index.py
+++ LAMBDA/index.py
@@ -171,13 +171,7 @@ def get_competitions(intent, current_time):
                 </speak> 
             ''',
             '''
-                <speak>
-                %s has started since %s,
-                <say-as interpret-as='date'>????%s</say-as>,
-                and will end at %s,
-                <say-as interpret-as='date'>????%s</say-as>,
-                in %s.com
-                </speak> 
+                <speak>%s has started since %s,<say-as interpret-as='date'>????%s</say-as>, and will end at %s, <say-as interpret-as='date'>????%s</say-as>, in %s.com </speak> 
             '''
             ]
 
