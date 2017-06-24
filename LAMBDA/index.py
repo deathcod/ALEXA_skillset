fd75c100765b20945074e66128e600bd463e74da
deathcod <chinmay.rakshit@gmail.com>
2017-06-24 19:36:15 +0530

    changing the speech output

--- LAMBDA/index.py
+++ LAMBDA/index.py
@@ -192,6 +192,7 @@ def get_competitions(intent, current_time):
             
             start_time = time.strftime('%d-%h ,%I:%M %p',time.gmtime(i["start_time"]))
             end_time = time.strftime('%d-%h ,%I:%M %p',time.gmtime(i["end_time"]))
+            i["competiton_name"] = i["competition_name"].replace(':', '')
 
             #reference process_query.json    
             speech_output += reply[1] %(i["competiton_name"]        #competiton_name
