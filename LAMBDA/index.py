7a5263e59684d10d340e29835f69c8bfdebd32d5
deathcod <chinmay.rakshit@gmail.com>
2017-07-01 20:38:30 +0530

    changing the speech output

--- LAMBDA/index.py
+++ LAMBDA/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import os, sys
+
+import os
+import sys
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR))
@@ -11,18 +13,19 @@ from datetime import datetime
 import time
 import re
 
+
 # --------------- Helpers that build all of the responses ----------------------
 
 # this function filters the text and makes it acceptable by the SSML.
 def SSML_filter(text):
+    # replace the key in the text with the value
+    replacement = {"&": "and", "\n": "", "[ ]+": " "}
 
-    #replace the key in the text with the value
-    replacement = {"&" : "and" ,"\n" : "", "[ ]+" : " "}
-
-    for key,value in replacement.items():
+    for key, value in replacement.items():
         text = re.sub(key, value, text)
 
     return text
+
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
     return {
@@ -56,7 +59,6 @@ def build_response(session_attributes, speechlet_response):
 # --------------- Functions that control the skill's behavior ------------------
 
 def help_response():
-
     session_attributes = {}
     card_title = "Help"
     speech_output = '''
@@ -75,7 +77,6 @@ def help_response():
 
 
 def invalid_response():
-
     session_attributes = {}
     card_title = "Invalid Response"
     speech_output = '''
@@ -94,6 +95,7 @@ def invalid_response():
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
+
 def get_welcome_response():
     """ If we wanted to initialize the session to have some attributes we could
     add those here
@@ -105,7 +107,7 @@ def get_welcome_response():
                     Welcome to the CodeGeek. Please say, "get two upcoming coding competitions",
                     to know the latest upcoming coding contests. Else, for more information please say "help"
                     '''
-    
+
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
     reprompt_text = '''
@@ -129,6 +131,7 @@ def handle_session_end_request():
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
+
 
 # --------------- Events ------------------
 
@@ -157,8 +160,8 @@ def get_competitions(intent, current_time):
     current_time = datetime.strptime(current_time, "%Y-%m-%dT%H:%M:%SZ")
     current_time -= epoch
     current_time = int(current_time.total_seconds())
-    x = {'count' : 1, 'status' : 'recent', 'type': 'all', 'DEPLOY': True, 'now': current_time}
-    
+    x = {'count': 1, 'status': 'recent', 'type': 'all', 'DEPLOY': True, 'now': current_time}
+
     if 'value' in intent['slots']['fetch']:
         if intent['slots']['fetch']['value'] not in ['load', 'get', 'fetch', 'retrieve']:
             return invalid_response()
@@ -167,12 +170,11 @@ def get_competitions(intent, current_time):
         x['count'] = int(intent['slots']['count']['value'])
         if x['count'] not in [1, 2, 3]:
             return invalid_response()
-    
+
     if 'value' in intent['slots']['type']:
         x['type'] = intent['slots']['type']['value']
         if x['type'] not in ['all', 'hackathon', 'coding', 'algorithmic', 'competitive programming']:
             return invalid_response()
-
 
     if 'value' in intent['slots']['status']:
         x['status'] = intent['slots']['status']['value']
@@ -181,40 +183,39 @@ def get_competitions(intent, current_time):
 
     response = Fetch(**x)
 
-    #reply structures
-    #TODO add more structures in future.
-    reply = ["%s will start at %s, in %s.com .", "%s has started since %s, and will end at %s, in %s.com ." ]
+    # reply structures
+    # TODO add more structures in future.
+    reply = ["%s will start at %s, in %s.com .", "%s has started since %s, and will end at %s, in %s.com ."]
 
     session_attributes = {}
     card_title = x['type'] + " contest"
-    
+
     speech_output = ""
     if x['status'] == 'ongoing':
         for i in response:
-            
-            start_time = time.strftime('%d-%h ,%I:%M %p',time.gmtime(i["start_time"]))
-            end_time = time.strftime('%d-%h ,%I:%M %p',time.gmtime(i["end_time"]))
+            start_time = time.strftime('%d-%h ,%I:%M %p', time.gmtime(i["start_time"]))
+            end_time = time.strftime('%d-%h ,%I:%M %p', time.gmtime(i["end_time"]))
 
-            #reference process_query.json    
-            speech_output += SSML_filter(reply[1] %(i["competiton_name"] #competiton_name
-                                       ,start_time
-                                       ,end_time
-                                       ,i['site_name']              #site_name
-                                       ))
+            # reference process_query.json
+            speech_output += SSML_filter(reply[1] % (i["competiton_name"]  # competiton_name
+                                                     , start_time
+                                                     , end_time
+                                                     , i['site_name']  # site_name
+                                                     ))
 
     else:
         for i in response:
-            start_time = time.strftime('%d-%h ,%I:%M %p',time.gmtime(i["start_time"]))
+            start_time = time.strftime('%d-%h ,%I:%M %p', time.gmtime(i["start_time"]))
 
-            #reference process_query.json  
-            speech_output += SSML_filter(reply[0] %(i["competiton_name"]     #competiton_name
-                                        ,start_time
-                                        ,i['site_name']             #site_name
-                                        ))
-    
+            # reference process_query.json
+            speech_output += SSML_filter(reply[0] % (i["competiton_name"]  # competiton_name
+                                                     , start_time
+                                                     , i['site_name']  # site_name
+                                                     ))
+
     reprompt_text = "I know I speak too fast, but you can follow me if you lower down your query count."
 
-    should_end_session = True
+    should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
@@ -236,7 +237,7 @@ def on_intent(intent_request, session):
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
         return handle_session_end_request()
     elif intent_name == "GetCompetitions":
-        return get_competitions(intent,intent_request['timestamp'])
+        return get_competitions(intent, intent_request['timestamp'])
     else:
         return invalid_response()
 
@@ -283,11 +284,11 @@ def lambda_handler(event, context):
 
 def testing_lambda():
     x = ""
-    with open( CURRENT_DIR +'/testing/input1.json','r') as f:
+    with open(CURRENT_DIR + '/testing/input1.json', 'r') as f:
         x = json.loads(f.read())
         pass
-    print (lambda_handler(x, " "))
+    print(lambda_handler(x, " "))
     pass
 
-#comment it out while transfering to the LAMBDA
-#testing_lambda()
+    # comment it out while transfering to the LAMBDA
+    # testing_lambda()
